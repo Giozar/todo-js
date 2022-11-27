f14ea697a30b58cd01c25e7f460fc2619afe8530
giovanni <manuelgio.cortazar.delacruz@gmail.com>
2022-11-26 23:22:53 -0600

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-import { saludar} from './js/componentes';
 import './styles.css';
-const nombre = 'giovanni';
 
-saludar( nombre );
+import {Todo, TodoList} from './classes/index.js';
+import { crearTodoHtml } from './js/componentes';
+
+// import { Todo } from './classes/todo.class.js';
+// import { TodoList } from './classes/todo-list.class.js';
+
+export const todoList = new TodoList();
+
+// const tarea = new Todo('Aprender Javascript!!!');
+// todoList.nuevoTodo( tarea );
+
+// console.log( todoList );
+// crearTodoHtml( tarea );
+
+// localStorage.setItem('mi-key','ABC123');
+// sesisonStorage.setItem('mi-key','ABC123');
+
+// setTimeout(()=>{
+//     localStorage.removeItem
+// }, 1500);
+
+todoList.todos.forEach(todo => crearTodoHtml(todo));
+// todoList.todos.forEach(crearTodoHtml); pro tip2
+
+console.log('todos', todoList.todos);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,18 +1,117 @@
-import '../css/componentes.css'
-// import webpacklogo from '../assets/img/webpack-logo.png';
-export const saludar = ( nombre ) => {
+import { Todo } from '../classes';
+import { todoList } from '../index';
+// Referencia en el HTML
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchoFiltros = document.querySelectorAll('.filtro') 
+export const crearTodoHtml = ( todo ) =>{
 
-    console.log('crando etiqueta');
+    const htmlTodo = `
+    <li class="${ (todo.completado ) ? 'completed ' : '' }" data-id="${ todo.id }">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${ (todo.completado ) ? 'checked ' : '' }>
+            <label>${ todo.tarea }</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
 
-    const h1 = document.createElement('h1');
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
 
-    h1.innerText = `Hola, ${ nombre  }!!!`;
+    divTodoList.append(div.firstElementChild);
 
-    document.body.append(h1);
+    return div;
+}  
 
-    //img
-    // const img = document.createElement('img');
-    // img.src = webpacklogo;
-    // document.body.append(img);
+//Eventos
+txtInput.addEventListener('keyup', ( event ) => {
 
-}
+    if ( event.keyCode === 13 && txtInput.value.length > 0){
+        // console.log(txtInput.value);
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo ( nuevoTodo);
+
+        //console.log(todoList);
+        crearTodoHtml ( nuevoTodo);
+        txtInput.value = '';
+    }
+});
+
+divTodoList.addEventListener('click', (event)=>{
+
+    const nombreElemento = event.target.localName; //input, label, button
+    const todoElemento = event.target.parentElement.parentElement;
+    const todoId = todoElemento.getAttribute('data-id');
+    //console.log(todoElemento);
+    //console.log(todoId);
+
+    // console.log(nombreElemento);
+
+    if( nombreElemento.includes('input')){ //click en el check
+
+        todoList.marcarCompletado( todoId );
+        todoElemento.classList.toggle('completed');
+    } else if(nombreElemento.includes('button')){ // hay que borrar el todo
+
+        todoList.eliminarTodo( todoId );
+        divTodoList.removeChild( todoElemento);        
+    } 
+    //console.log(todoList);
+    
+    btnBorrar.addEventListener('click', () => {
+        todoList.eliminarCompletados();
+
+        for( let i = divTodoList.children.length-1; i >= 0; i --){
+
+            const elemento = divTodoList.children[i];
+
+            //console.log(elemento);
+
+            if(elemento.classList.contains('completed')){
+                divTodoList.removeChild(elemento);
+            }
+
+
+        }
+    });
+});
+
+ulFiltros.addEventListener('click', (event) => {
+    // console.log(event.target.text);
+
+    const filtro = event.target.text;
+    if( !filtro ) {return; }
+
+    anchoFiltros.forEach( elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+
+    for (const elemento of divTodoList.children) {
+        
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+
+        switch ( filtro){
+            case 'Pendientes':
+                if( completado ){
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+
+            case 'Completados':
+                if( !completado ){
+                    elemento.classList.add('hidden');
+                }
+            break;
+
+
+
+
+        }
+    }
+
+
+})
